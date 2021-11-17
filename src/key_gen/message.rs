7906cbba0b1099ed95afe4df6ebf9b243c519783
qima <qi.ma@maidsafe.net>
2021-11-17 21:38:06 +0800

    feat: DKG AE patten refactory

--- src/key_gen/message.rs
+++ src/key_gen/message.rs
@@ -12,7 +12,11 @@ use super::{Acknowledgment, Part};
 use serde_derive::{Deserialize, Serialize};
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt;
+use tiny_keccak::{Hasher, Sha3};
 use xor_name::XorName;
+
+/// SHA3-256 hash digest.
+type Digest256 = [u8; 32];
 
 /// Messages used for running BLS DKG.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
@@ -41,6 +45,32 @@ pub enum Message {
         key_gen_id: u64,
         ack: Acknowledgment,
     },
+}
+
+impl Message {
+    // Creator of the message.
+    pub fn creator(&self) -> u64 {
+        match &*self {
+            Message::Initialization { key_gen_id, .. }
+            | Message::Proposal { key_gen_id, .. }
+            | Message::Complaint { key_gen_id, .. }
+            | Message::Justification { key_gen_id, .. }
+            | Message::Acknowledgment { key_gen_id, .. } => *key_gen_id,
+        }
+    }
+
+    // Identifier of the message.
+    pub fn id(&self) -> XorName {
+        let mut hasher = Sha3::v256();
+        let mut hash = Digest256::default();
+
+        if let Ok(serialized) = bincode::serialize(self) {
+            hasher.update(&serialized);
+            hasher.finalize(&mut hash);
+        }
+
+        XorName::from_content(&hash)
+    }
 }
 
 impl fmt::Debug for Message {
